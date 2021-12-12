559a8970828dcd19d07d05a7f333e37755c0ac43
chaenabi <find1086@gmail.com>
2021-12-12 17:07:55 +0900

    Add Flux example

---
+++ src/main/java/com/company/springreactive/basic/FluxSchedulerEx.java
@@ -0,0 +1,30 @@
+package com.company.springreactive.basic;
+
+import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Flux;
+import reactor.core.scheduler.Schedulers;
+
+import java.time.Duration;
+
+/**
+ * spring-web-flux example
+ * @date 2021-12-12
+ * @author Informix
+ */
+@Slf4j
+public class FluxSchedulerEx {
+
+    public static void main(String[] args) {
+        Flux.range(1, 10)
+                .publishOn(Schedulers.newSingle("publisher"))
+                .log()
+                .subscribeOn(Schedulers.newSingle("subcriber"))
+                .subscribe(e -> log.info(e + " times scheduler run."));
+
+        log.info("Exit");
+
+        Flux.interval(Duration.ofMillis(500))
+                .subscribe(e -> log.info("onNext: {}", e));
+
+    }
+}
--- src/main/java/com/company/springreactive/basic/SchedulerEx.java
+++ src/main/java/com/company/springreactive/basic/SchedulerEx.java
@@ -1,10 +1,14 @@
 package com.company.springreactive.basic;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.concurrent.CustomizableThreadFactory;
+
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Flow;
 
 // Reactive Streams - Scheduler
+@Slf4j
 public class SchedulerEx {
     public static void main(String[] args) {
 
@@ -21,36 +25,82 @@ public class SchedulerEx {
 
             @Override
             public void cancel() {
-                System.out.println("cancel");
+                log.info("cancel");
             }
         });
 
         Flow.Publisher<Integer> subOnPub = sub -> {
-            ExecutorService es = Executors.newSingleThreadExecutor();
+
+            ExecutorService es = Executors.newSingleThreadExecutor(new CustomizableThreadFactory() {
+                @Override
+                protected String getDefaultThreadNamePrefix() {
+                    return "subOn-";
+                }
+            });
+
             es.execute(() -> pub.subscribe(sub));
+            es.shutdown();
         };
 
-        pub.subscribe(new Flow.Subscriber<>() {
+        Flow.Publisher<Integer> pubOnPub = sub -> {
+
+            ExecutorService es = Executors.newSingleThreadExecutor(new CustomizableThreadFactory() {
+                @Override
+                protected String getDefaultThreadNamePrefix() {
+                    return "pubOn -";
+                }
+            });
+
+            es.execute(() -> subOnPub.subscribe(new Flow.Subscriber<>() {
+                @Override
+                public void onSubscribe(Flow.Subscription subscription) {
+                    sub.onSubscribe(subscription);
+                }
+
+                @Override
+                public void onNext(Integer item) {
+                    sub.onNext(item);
+                }
+
+                @Override
+                public void onError(Throwable throwable) {
+                    sub.onError(throwable);
+                    es.shutdown();
+                }
+
+                @Override
+                public void onComplete() {
+                    sub.onComplete();
+                    es.shutdown();
+                }
+            }));
+        };
+
+        Flow.Subscriber<Integer> subscriber = new Flow.Subscriber<>() {
             @Override
             public void onSubscribe(Flow.Subscription subscription) {
-                System.out.println("onSubscribe");
+                log.info("onSubscribe");
                 subscription.request(Long.MAX_VALUE);
             }
 
             @Override
             public void onNext(Integer item) {
-                System.out.println("item : " + item);
+                log.info("item : " + item);
             }
 
             @Override
             public void onError(Throwable throwable) {
-                System.out.println("onError");
+                log.info("onError");
             }
 
             @Override
             public void onComplete() {
-                System.out.println("onComplete");
+                log.info("onComplete");
             }
-        });
+        };
+
+        //pub.subscribe(subscriber);
+        pubOnPub.subscribe(subscriber);
+
     }
 }
