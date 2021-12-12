bdc301fa88d5113cf516076ce925148795f7111a
chaenabi <find1086@gmail.com>
2021-12-12 12:50:03 +0900

    Change Scheduler example

--- src/main/java/com/company/springreactive/basic/SchedulerEx.java
+++ src/main/java/com/company/springreactive/basic/SchedulerEx.java
@@ -1,31 +1,56 @@
 package com.company.springreactive.basic;
 
-import io.reactivex.rxjava3.core.Observable;
-import io.reactivex.rxjava3.schedulers.Schedulers;
-
-import java.util.Collections;
-import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Flow;
 
 // Reactive Streams - Scheduler
-
-/**
- * Schedulers.computation() 을 이용해서 계산 작업을 처리하는 예제
- */
 public class SchedulerEx {
     public static void main(String[] args) {
-        Observable<Integer> observable1 = Observable.fromIterable(List.of(30, 40, 50, 60, 70,
-                                                                    80, 90, 100, 110, 120, 130,
-                                                                    140, 150, 160, 170, 180,
-                                                                    190, 200, 210, 220, 230, 240, 250, 260,
-                                                                    270, 280, 290, 300));
-        Observable<Integer> observable2 = Observable.range(1, 24);
 
-        Observable<Object> source = Observable.zip(observable1, observable2, (data1, hour) -> hour + "시: " + Collections.max(List.of(data1)));
+        Flow.Publisher<Integer> pub = (subscriber) -> subscriber.onSubscribe(new Flow.Subscription() {
+            @Override
+            public void request(long n) {
+                subscriber.onNext(1);
+                subscriber.onNext(2);
+                subscriber.onNext(3);
+                subscriber.onNext(4);
+                subscriber.onNext(5);
+                subscriber.onComplete();
+            }
 
-        source.subscribeOn(Schedulers.computation())
-                .subscribe(System.out::println);
+            @Override
+            public void cancel() {
+                System.out.println("cancel");
+            }
+        });
 
-        source.subscribe();
-        //TimeUtil.sleep(500L);
+        Flow.Publisher<Integer> subOnPub = sub -> {
+            ExecutorService es = Executors.newSingleThreadExecutor();
+            es.execute(() -> pub.subscribe(sub));
+        };
+
+        pub.subscribe(new Flow.Subscriber<>() {
+            @Override
+            public void onSubscribe(Flow.Subscription subscription) {
+                System.out.println("onSubscribe");
+                subscription.request(Long.MAX_VALUE);
+            }
+
+            @Override
+            public void onNext(Integer item) {
+                System.out.println("item : " + item);
+            }
+
+            @Override
+            public void onError(Throwable throwable) {
+                System.out.println("onError");
+            }
+
+            @Override
+            public void onComplete() {
+                System.out.println("onComplete");
+            }
+        });
     }
 }
